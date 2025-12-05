dee289e0a20ceda0ceeb6986f7b348f1ed3c1121
DarkIron1124 <darkiron1124@gmail.com>
2025-12-04 23:33:23 -0700

    added recalculate function

--- lab-js-PROVIDED/scripts/scripts.js
+++ lab-js-PROVIDED/scripts/scripts.js
@@ -1,4 +1,4 @@
-/****************** YOUR NAME: 
+/****************** YOUR NAME: ~
 
 The instructions describe the missing logic that is needed; you will translate these into JavaScript in the places indicated.
 
@@ -11,6 +11,8 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
+let modelName = "";
+let duration = 0;
 
 
 
@@ -27,7 +29,18 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
+function recalculate() {
+    let costLabel = document.getElementById("calculated-cost")
+    let totalCost = 0;
 
+    if (modelName == "XYZ") {
+        totalCost = 100 * duration;
+    } else if (modelName == "CPRG") {
+        totalCost = 213 * duration;
+    }
+
+    costLabel.innerHTML = `${totalCost.toFixed(2)}`;
+}
 
 
 
@@ -46,7 +59,10 @@ You are encouraged to use the provided naming convention for ease of review.
 
 // INSERT YOUR CODE HERE
 
+function changeModel() {
+    let modelLabel = document.getElementById("model-text");
 
+}
 
 
 
